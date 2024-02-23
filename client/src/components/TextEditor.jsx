fa055c7c4911489a46cf7e07293c41df356f4d76
Karan-Palan <karanpalan007@gmail.com>
2024-02-23 19:38:36 +0530

    Basic structure done

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,9 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
 
 const TextEditor = () => {
+  const [socket, setSocket] = useState(null);
+  const [quill, setQuill] = useState(null);
   const [value, setValue] = useState("");
+
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    setSocket(socket);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on("receive-changes", handler);
+
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
+
+  const handleEditorChange = (content, delta, source, editor) => {
+    if (source === "user") {
+      // Changes originated from this client, emit them to the server
+      setValue(content);
+      socket.emit("send-changes", delta);
+    } else if (source === "api") {
+      // Changes originated from applying remote changes, update the editor
+      setValue(content);
+    }
+  };
+
+  const handleQuillRef = (ref) => {
+    if (ref) {
+      setQuill(ref.getEditor());
+    }
+  };
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-violet-400 to-blue-900 flex flex-col justify-center items-center">
@@ -12,8 +54,9 @@ const TextEditor = () => {
         <ReactQuill
           theme="snow"
           value={value}
-          onChange={setValue}
+          onChange={handleEditorChange}
           className="h-full"
+          ref={handleQuillRef}
         />
       </div>
     </div>
